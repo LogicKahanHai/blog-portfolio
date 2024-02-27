4a1631eda354aea982ac40d329018b87c46f2597
Logic - Rishi Bhalla <contact.logickahanhai@gmail.com>
2024-02-27 12:46:41 +0530

    v0.0.1 Prod

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center flex-col items-center'>
+        <iframe src="https://giphy.com/embed/H1dxi6xdh4NGQCZSvz" width="320" height="320" className="giphy-embed pointer-events-none"></iframe>
+
+        <h3 className='text-3xl font-bold mt-10'>The Developer is working on the website <span className='underline'>day and night!</span></h3>
+        <h3 className='text-4xl font-bold mt-10'>Please visit the site on <span className='underline'>9th March, 2024</span> for a finished product.</h3>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
