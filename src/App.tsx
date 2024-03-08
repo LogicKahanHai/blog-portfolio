2c542357c51f53b79579fb11446b6735cb5a5f02
Logic - Rishi Bhalla <contact.logickahanhai@gmail.com>
2024-03-08 16:04:10 +0530

    Updated gif and date.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <>
       <div className='flex justify-center flex-col items-center'>
-        <iframe src="https://giphy.com/embed/H1dxi6xdh4NGQCZSvz" width="320" height="320" className="giphy-embed pointer-events-none"></iframe>
+        <img src="/cat.gif" alt="cat.gif" height="320" width="320" />
 
         <h3 className='text-3xl font-bold mt-10'>Logic is working on the website <span className='underline'>day and night!</span></h3>
-        <h3 className='text-4xl font-bold mt-10'>Please visit again on  <span className='underline'>9th March, 2024</span> for a finished product.</h3>
+        <h3 className='text-4xl font-bold mt-10'>Please visit again on  <span className='underline'>23rd March, 2024</span> for a finished product.</h3>
 
 
       </div>
