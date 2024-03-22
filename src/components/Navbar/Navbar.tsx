84cbed9b2dce3621dccab1679fc6e2458c99be65
Logic - Rishi Bhalla <contact.logickahanhai@gmail.com>
2024-03-22 15:21:41 +0530

    Updated the Navbar for scroll animations

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,43 @@
-export default function Navbar() {
+import React, { useEffect, useState } from "react";
+import { Link as PageLink } from "react-router-dom";
+import { Link as HomeScrollLink, animateScroll as scroll } from "react-scroll";
+
+export default function Navbar(): React.ReactElement {
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  const [isHome, setIsHome] = useState(false);
+
+  const pathName = window.location.pathname;
+
+  useEffect(() => {
+    if (pathName === "/") {
+      setIsHome(true);
+    } else {
+      setIsHome(false);
+    }
+  }, [pathName]);
+
   return (
     <>
-      {/* phone
-            <header className="sticky top-0 left-0 right-0 z-50 tablet:hidden bigPhone:flex">
-                <div className="flex justify-between items-center w-full h-16 bg-white shadow-md px-5">
-                    <div className="text-xl font-bold text-gray-800 cursor-pointer">Logic Kahan Hai?</div>
-                    <svg className="h-6 w-6 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                </div>
-            </header> */}
-
       {/* bigPhone */}
       <header className="tablet:hidden phone:flex font-display sticky left-0 right-0 top-0 z-50">
         <div className="flex h-16 w-full items-center justify-between bg-white px-5 shadow-md">
           <div className="cursor-pointer text-xl font-bold text-gray-800">
-            Logic Kahan Hai?
+            {!isHome && <PageLink to="/">Logic Kahan Hai?</PageLink>}
+            {isHome && (
+              <HomeScrollLink
+                to="/"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                onClick={scrollToTop}
+              >
+                Logic Kahan Hai?
+              </HomeScrollLink>
+            )}
           </div>
           {/* <svg className="h-6 w-6 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -27,47 +49,119 @@ export default function Navbar() {
       <header className="tablet:flex font-display sticky left-0 right-0 top-0 z-50 hidden">
         <div className="flex w-full items-center justify-between bg-white px-20 py-8 shadow-md">
           {/* <div className="flex items-center gap-5"> */}
-          <div className="cursor-pointer text-2xl font-bold text-gray-800">
-            Logic Kahan Hai?
-          </div>
+          {!isHome && (
+            <PageLink to="/">
+              <div className="cursor-pointer text-2xl font-bold text-gray-800">
+                Logic Kahan Hai?
+              </div>
+            </PageLink>
+          )}
+          {isHome && (
+            <HomeScrollLink
+              to="/"
+              spy={true}
+              smooth={true}
+              offset={-200}
+              duration={700}
+              onClick={scrollToTop}
+            >
+              <div className="cursor-pointer text-2xl font-bold text-gray-800">
+                Logic Kahan Hai?
+              </div>
+            </HomeScrollLink>
+          )}
           <div className="flex items-center gap-5">
-            <a
-              href="#home"
-              className="text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600"
-            >
-              Home
-            </a>
-            <a
-              href="#about"
-              className="text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600"
-            >
-              About
-            </a>
-            <a
-              href="#projects"
-              className="text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600"
-            >
-              Projects
-            </a>
-            <a
-              href="#blogs"
-              className="text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600"
-            >
-              Blogs
-            </a>
-            <a
-              href="#contact"
-              className="text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600"
-            >
-              Contact
-            </a>
-            {/* </div> */}
+            {/* Home LI */}
+
+            <div className="cursor-pointer text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600">
+              {isHome ? (
+                <HomeScrollLink
+                  to="/"
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={700}
+                  onClick={scrollToTop}
+                >
+                  Home
+                </HomeScrollLink>
+              ) : (
+                <PageLink to="/">Home</PageLink>
+              )}
+            </div>
+
+            {/* About LI */}
+
+            <div className="cursor-pointer text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600">
+              {isHome ? (
+                <HomeScrollLink
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={700}
+                >
+                  About
+                </HomeScrollLink>
+              ) : (
+                <PageLink to="/#about">About</PageLink>
+              )}
+            </div>
+
+            {/* Projects LI */}
+
+            <div className="cursor-pointer text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600">
+              {isHome ? (
+                <HomeScrollLink
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={700}
+                >
+                  Projects
+                </HomeScrollLink>
+              ) : (
+                <PageLink to="/#projects">Projects</PageLink>
+              )}
+            </div>
+
+            {/* Blogs LI */}
+
+            <div className="cursor-pointer text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600">
+              {isHome ? (
+                <HomeScrollLink
+                  to="blogs"
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={700}
+                >
+                  Blogs
+                </HomeScrollLink>
+              ) : (
+                <PageLink to="/#blogs">Blogs</PageLink>
+              )}
+            </div>
+
+            {/* Contact LI */}
+
+            <div className="cursor-pointer text-lg font-bold  text-gray-600 transition-all duration-300 ease-in-out hover:text-indigo-600">
+              {isHome ? (
+                <HomeScrollLink
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={-200}
+                  duration={700}
+                >
+                  Contact
+                </HomeScrollLink>
+              ) : (
+                <PageLink to="/#contact">Contact</PageLink>
+              )}
+            </div>
           </div>
-          {/* <div className="flex items-center gap-5">
-                <a href="" className="text-lg font-medium text-gray-600 hover:text-indigo-600 transition duration-300 ease-in-out">Resume</a>
-                <a href="" className="text-lg font-medium text-gray-600 hover:text-indigo-600 transition duration-300 ease-in-out">Github</a>
-                <a href="" className="text-lg font-medium text-gray-600 hover:text-indigo-600 transition duration-300 ease-in-out">LinkedIn</a>
-            </div> */}
         </div>
       </header>
     </>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import { LandingComponent } from "../../components/HomeLanding/home";
 
 const Home = () => {
-    return (
-        <div className="overflow-x-clip">
-            <LandingComponent />
-        </div>
-    );
-}
+  return (
+    <div id="landing" className="overflow-x-clip">
+      <LandingComponent />
+    </div>
+  );
+};
 
 export default Home;
