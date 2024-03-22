ab417102c90a5da7cec7e47527aa661ccfd917ff
Logic - Rishi Bhalla <contact.logickahanhai@gmail.com>
2024-03-22 13:49:33 +0530

    Updated project state and launch date.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='flex justify-center flex-col items-center'>
+      <div className="flex flex-col items-center justify-center">
         <img src="/cat.gif" alt="cat.gif" height="320" width="320" />
 
-        <h3 className='text-3xl font-bold mt-10'>Logic is working on the website <span className='underline'>day and night!</span></h3>
-        <h3 className='text-4xl font-bold mt-10'>Please visit again on  <span className='underline'>23rd March, 2024</span> for a finished product.</h3>
-
-
+        <h3 className="mt-10 text-3xl font-bold">
+          Logic is working on the website{" "}
+          <span className="underline">day and night!</span>
+        </h3>
+        <p className="mt-5 text-xl">
+          The website is now in the testing phase and will be made live soon.
+          <br />
+          Sorry for the inconvenience.
+        </p>
+        <h3 className="mt-10 text-4xl font-bold">
+          Please visit again on{" "}
+          <span className="underline">25th March, 2024</span> for a finished
+          product.
+        </h3>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
